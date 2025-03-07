3a991cc70d8da4782da77b9e0252747840db60e2
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-03-07 14:53:38 -0800

    Removed file storage and path codes

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,7 +1,14 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+document.addEventListener('DOMContentLoaded', function() {
+    FilePond.registerPlugin(FilePondPluginImagePreview);
+    FilePond.registerPlugin(FilePondPluginImageResize);
+    FilePond.registerPlugin(FilePondPluginFileEncode);
+    const inputElement = document.querySelector('input[type="file"]');
+    const pond = FilePond.create(inputElement);
+    FilePond.parse(document.body);
+  });  
 
-FilePond.parse(document.body)
+  FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+  })
