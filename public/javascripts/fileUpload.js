41e73e7bed9e9083cedc72fcaf5a3262d02ff066
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-03-11 08:58:23 -0700

    Finished Project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+  rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
+  ready()
+} else {
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-large'))
+  const coverHeight =  coverWidth / coverAspectRatio
+
+  document.addEventListener('DOMContentLoaded', function() {
     FilePond.registerPlugin(FilePondPluginImagePreview);
     FilePond.registerPlugin(FilePondPluginImageResize);
     FilePond.registerPlugin(FilePondPluginFileEncode);
@@ -8,7 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
   });  
 
   FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
   })
+}
