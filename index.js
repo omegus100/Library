e0b3dc142b17be944d6f12f78ebee6b82b3f6676
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-03-25 10:21:32 -0700

    Added path variable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app =  express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride =  require('method-override')
-
+const path = require('path')
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
